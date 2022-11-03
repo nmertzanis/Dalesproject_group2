b9092256937c60fde3970115644bf5a2ea920b50
apieruzzi <104371681+apieruzzi@users.noreply.github.com>
2022-11-03 16:56:04 +0100

    Final working script to produce prof.inp

--- scripts for prof.inp/data_processing.py
+++ scripts for prof.inp/data_processing.py
@@ -8,7 +8,7 @@ Created on Fri Oct 28 14:40:59 2022
 "TODO"
 # - Check height corresponds to right values -> what is ASTEX? 
 # - Check interpolation -> use vertical grid they used in the radtransf case (import that file and see how it works)
-# - Fix TKE 
+# - Fix hbl for TKE 
 # - Fix plots and check everything makes sense
 # - Write input files 
 # - Run simulation
@@ -54,10 +54,10 @@ for i in range (len(rel_time)):
     dateList.append(date_1 + datetime.timedelta(days=rel_time[i]))
 
 #Selected days for initial observations crossing with CCN plots:
-day_min = '28/02/09 12'
-day_max = '28/05/09'
+day_min = '06/01/09 12'
+day_max = '03/04/09 12'
 date_min = datetime.datetime.strptime(day_min, "%d/%m/%y %H")
-date_max = datetime.datetime.strptime(day_max, "%d/%m/%y")
+date_max = datetime.datetime.strptime(day_max, "%d/%m/%y %H")
 
 ind_min = np.where(np.array(dateList) == date_min)[0][0]
 ind_max = np.where(np.array(dateList) == date_max)[0][0]
@@ -128,6 +128,19 @@ for i in range(len(ind)):
     wd_interp[i] = interpolate(height[i], wd[i], z, linear=False)
     ws_interp[i] = interpolate(height[i], ws[i], z, linear=False)
 
+#Save original data (to plot later) and change names of interpolated ones (to use for calculations)
+temp_o = temp
+DPD_o = DPD
+P_o = P
+ws_o = ws
+wd_o = wd
+
+
+temp = temp_interp
+DPD = DPD_interp
+P = P_interp
+ws = ws_interp
+wd = wd_interp
 
 "CALCULATIONS"
 
@@ -164,16 +177,16 @@ V = ws*np.cos(np.deg2rad(wd))
 
 Cd = 0.005 #drag coefficient for praire
 h_bl = 1000 #change manually depending on where the inversion jump is
-w10 = U[:,0] #wind speed at 2m (should be at 10m, use interp once available)
+w10 = U[:,1] #wind speed at 7.5m (should be at 10m, approximation)
 u_st = np.sqrt(Cd * w10**2)
-Up = np.zeros(height.shape)
-Vp = np.zeros(height.shape)
+Up = np.zeros(dim)
+Vp = np.zeros(dim)
 
 for i in range(len(u_st)):
-    for j in range(0,x,1):
-        if height[i,j]<=h_bl:
-            Up[i,j] = 2*u_st[i]*(1-(height[i,j]/h_bl))**(3/4)
-            Vp[i,j] = 2.2*u_st[i]*(1-(height[i,j]/h_bl))**(3/4)
+    for j in range(len(z)):
+        if z[j]<=h_bl:
+            Up[i,j] = 2*u_st[i]*(1-(z[j]/h_bl))**(3/4)
+            Vp[i,j] = 2.2*u_st[i]*(1-(z[j]/h_bl))**(3/4)
         else:
             Up[i,j] = 0
             Vp[i,j] = 0
@@ -181,110 +194,145 @@ for i in range(len(u_st)):
 tke = 1/2 * (Up**2 + Vp**2)
 
 
-
-"CREATING INPUT FILES"
-
-profile = open("inputfiles/prof.inp.001.txt", "w")
-profile.write(" Dry Convective Boundary Layer LES Course Class 1\n \
-     height(m)   thl(K)     qt(kg/kg)       u(m/s)     v(m/s)     tke(m2/s2)\n")
-
-
-def format_num(n):
-    return '{:.7s}'.format('{:0.4f}'.format(n))
-
-def add_line(height, thl, qt, u, v, tke):
-
-    return "      " + format_num(height) + "      " \
-    + format_num(thl) + "      " + format_num(qt) + "      " \
-    + format_num(u) + "      " + format_num(v) + "      " \
-    + format_num(tke) + "\n"
-
-profile = open("inputfiles/prof.inp.001.txt", "a")
-
-
-#save to file
-for i in range(len(z)):
-    profile.write(add_line(z[i], interpthl[i], interpq[i], interpu[i], interpv[i], interptke[i]))
-
-
-
-
-
-
 "PLOTTING PROFILES"
 
 
 #plots
 
-plt.figure()
-plt.plot(DPD[ind_min,0:x],height[ind_min,0:x])
-plt.title("Dew point depression on day_min")
+# plt.figure()
+# plt.plot(DPD[0,:],z, label="day_min")
+# plt.plot(DPD[1,:],z, label="day_max")
+# plt.plot(DPD_o[0,0:5],height[0,0:5], 'o', label="day_min original")
+# plt.plot(DPD_o[1,0:5],height[1,0:5], 'o', label="day_max original")
+# plt.legend()
+# plt.title("Dew point depression")
 
 plt.figure()
-plt.plot(temp[ind_min,:],height[ind_min,:])
-plt.title("Temperature on day_min")
-plt.show()
-
-plt.figure()
-plt.plot(thl[ind_min,0:x],height[ind_min,0:x])
-plt.title("Liquid potential emperature on day_min")
-plt.show()
-
-plt.figure()
-plt.plot(RH[ind_min,0:x],height[ind_min,0:x])
-plt.title("Relative humidity on day_min")
-plt.show()
-
-plt.figure()
-plt.plot(q[ind_min,0:x],height[ind_min,0:x])
-plt.title("Total specific humidity on day_min")
-
-plt.figure()
-plt.plot(ql[ind_min,0:x],height[ind_min,0:x])
-plt.title("Liquid specific humidity on day_min")
-
-plt.figure()
-plt.plot(U[ind_min,0:x],height[ind_min,0:x])
-plt.title("Wind speed (U) on day_min")
-
-plt.figure()
-plt.plot(V[ind_min,0:x],height[ind_min,0:x])
-plt.title("Wind speed (U) on day_min")
-
-plt.figure()
-plt.plot(thl[ind_min,0:x],height[ind_min,0:x], label='thl')
-plt.plot(thl_man[ind_min,0:x],height[ind_min,0:x], label='thl_man')
-plt.title("Liquid potential emperature on day_min")
+plt.plot(P[0,:],z, label="day_min")
+plt.plot(P[1,:],z, label="day_max")
+plt.plot(P_o[0,0:5],height[0,0:5], 'o', label="day_min original")
+plt.plot(P_o[1,0:5],height[1,0:5], 'o', label="day_max original")
 plt.legend()
-plt.show()
+plt.title("Pressure")
 
 plt.figure()
-plt.plot(tke[ind_min,0:x],height[ind_min,0:x])
-plt.title("TKE on day_min")
+plt.plot(temp[0,:],z, label="day_min")
+plt.plot(temp[1,:],z, label="day_max")
+plt.plot(temp_o[0,0:5],height[0,0:5], 'o', label="day_min original")
+plt.plot(temp_o[1,0:5],height[1,0:5], 'o', label="day_max original")
+plt.legend()
+plt.title("Temperature")
 
-# plt.figure()
-# plt.plot(temp[ind_max,0:x],height[ind_max,0:x])
-# plt.title("Temperature on day_max")
-# plt.show()
+plt.figure()
+plt.plot(thl[0,:],z, label="day_min")
+plt.plot(thl[1,:],z, label="day_max")
+plt.title("Liquid potential temperature")
+plt.legend()
 
-# plt.figure()
-# plt.plot(thl[ind_max,0:x],height[ind_max,0:x])
-# plt.title("Liquid potential emperature on day_max")
-# plt.show()
+plt.figure()
+plt.plot(RH[0,:],z, label="day_min")
+plt.plot(RH[1,:],z, label="day_max")
+plt.title("Relative humidity")
+plt.legend()
 
-# plt.figure()
-# plt.plot(RH[ind_max,0:x],height[ind_max,0:x])
-# plt.title("Relative humidity on day_max")
-# plt.show()
+plt.figure()
+plt.plot(q[0,:],z, label="day_min")
+plt.plot(q[1,:],z, label="day_max")
+plt.legend()
+plt.title("Total specific humidity")
 
-# plt.figure()
-# plt.plot(q[ind_max,0:x],height[ind_max,0:x])
-# plt.title("Total specific humidity on day_max")
+plt.figure()
+plt.plot(U[0,:],z, label="day_min")
+plt.plot(U[1,:],z, label="day_max")
+plt.legend()
+plt.title("Wind speed (U)")
 
-# plt.figure()
-# plt.plot(DPD[ind_max,0:x],height[ind_max,0:x])
-# plt.title("Dew point depression on day_max")
+plt.figure()
+plt.plot(V[0,:],z, label="day_min")
+plt.plot(V[1,:],z, label="day_max")
+plt.legend()
+plt.title("Wind speed (V)")
 
-# plt.figure()
-# plt.plot(ql[ind_max,0:x],height[ind_max,0:x])
-# plt.title("Liquid specific humidity on day_max")
+plt.figure()
+plt.plot(tke[0,:],z, label="day_min")
+plt.plot(tke[1,:],z, label="day_max")
+plt.legend()
+plt.title("TKE")
+
+
+
+
+"CREATING INPUT FILES"
+
+
+# Profile for day_min
+profile = open("prof.inp.001.txt", "w")
+profile.write("#ASTEX case using prescribed vertical grid, Nlev = 427 1\n \
+      height(m)   thl(K)     qt(kg/kg)       u(m/s)     v(m/s)     tke(m2/s2)\n")
+
+
+def format_num(n):
+    return '{:.7s}'.format('{:0.4f}'.format(n))
+
+def format_q(q):
+    return "{:.3E}".format(q)
+
+def add_line(z, thl, qt, u, v, tke):
+
+    return "      " + format_num(z) + "      " \
+    + format_num(thl) + "      " + format_q(qt) + "      " \
+    + format_num(u) + "      " + format_num(v) + "      " \
+    + format_num(tke) + "\n"
+
+profile = open("prof.inp.001.txt", "a")
+
+
+#save to file
+for i in range(1,len(z),1):
+    profile.write(add_line(z[i], thl[0,i], q[0,i], U[0,i], V[0,i], tke[0,i]))
+
+
+
+# Profile for day_max
+profile = open("prof.inp.002.txt", "w")
+profile.write("#ASTEX case using prescribed vertical grid, Nlev = 427 1\n \
+      height(m)   thl(K)     qt(kg/kg)       u(m/s)     v(m/s)     tke(m2/s2)\n")
+
+profile = open("prof.inp.002.txt", "a")
+
+#save to file
+for i in range(1,len(z),1):
+    profile.write(add_line(z[i], thl[1,i], q[1,i], U[1,i], V[1,i], tke[1,i]))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
